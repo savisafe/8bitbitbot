f7b464706de3705f35413e57cfde89e2be58f6b2
savisafe <savisafe@vk.com>
2022-05-13 13:15:37 +0300

    update

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ icons.addEventListener("click", ()=>{
     icons.classList.add('display-none')
     play.classList.remove('display-none')
 })
+
 back.addEventListener("click", ()=>{
     play.classList.add('display-none')
     icons.classList.remove('display-none')
