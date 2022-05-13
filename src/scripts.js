23ca9cd184d2f530b41de0b1da91c0edf8d462e1
savisafe <savisafe@vk.com>
2022-05-13 13:02:19 +0300

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+const body = document.getElementById('body')
+const icons = document.getElementById('icons')
+const play = document.getElementById('play')
+const back = document.getElementById('back')
+icons.addEventListener("click", ()=>{
+    icons.classList.add('display-none')
+    play.classList.remove('display-none')
+})
+back.addEventListener("click", ()=>{
+    play.classList.add('display-none')
+    icons.classList.remove('display-none')
+})
